8260031db13ee9707bf5e21e2e9da86b5d5cc0aa
Daniel Kendell <daniel@kendell.org>
2015-06-09 01:26:02 +0100

    added a test for what happens when you set an array

--- test/DotTest.php
+++ test/DotTest.php
@@ -31,5 +31,20 @@ class DotTest extends \PHPUnit_Framework_TestCase {
     $this->assertEquals( 'you', $this->dot->get( 'over.write' ),
       "Getting foo.bar should have returned baz" );
   }
+
+  public function testSetArray() {
+    $array = [
+      'one' => [
+        'two' => 'three'
+      ]
+    ];
+    $this->dot->set( 'set.array', $array );
+
+    $this->assertEquals( $array, $this->dot->get( 'set.array' ),
+      "Getting set.array should have returned the original array" );
+
+    $this->assertEquals( 'three', $this->dot->get( 'set.array.one.two' ),
+      "Getting set.array.one.two should have returned 'three'" );
+  }
 }
 
