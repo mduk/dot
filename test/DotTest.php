445364461196cdb39b394771c64b97d2f816ab74
Daniel Kendell <daniel.kendell@gmail.com>
2017-02-05 00:20:56 +0000

    There's aleady a test for this, I just didn't spot it.

--- test/DotTest.php
+++ test/DotTest.php
@@ -63,13 +63,6 @@ class DotTest extends \PHPUnit_Framework_TestCase {
       "Getting array_access.foo should have returned 'yay foo!'" );
   }
 
-  public function testGetOverflow() {
-    try {
-      $this->dot->get( 'foo.bar.baz' );
-    }
-    catch ( Dot\Exception\DotOverflow $e ) {}
-  }
-
   public function testSet() {
     $return = $this->dot->set( 'foo.baz', 'bacon' );
 
